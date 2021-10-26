c62093f60d9e109b2ed21207ff8ef7d46acf1a0b
Iago Gondim <iagogondim200@gmail.com>
2021-10-26 11:04:31 -0300

    Possivel criar conta e não é possivel cadastrar um CPF já existente

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 const express = require("express");
 const app = express();
+const {v4: uuidv4} = require("uuid")
+const customers = [];
+app.use(express.json());
+
+
+app.post("/account", (request, response) => {
+    const {cpf, name}  = request.body;
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+if (customerAlreadyExists){
+    return response.status(400).json({error: "Customer already exists!"});
+}
+    const id = uuidv4();
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement:[]
+    });
+    return response.status(201).send();
+
+});
+
 
 
 app.listen(3333);
