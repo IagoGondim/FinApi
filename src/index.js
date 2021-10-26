b714ae9a327bf32802cd588035ab109dab7bf07c
Iago Gondim <iago-gondim@hotmail.com>
2021-10-26 14:09:00 -0300

    Criação do middleware

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ const {v4: uuidv4} = require("uuid")
 const customers = [];
 app.use(express.json());
 
+function verifyIfExistsAccountCPF(request, response, next) {
+    const {cpf} = request.headers;
+    
+    const customer = customers.find(customer => customer.cpf === cpf);
+    
+    if(!customer){
+        return response.status(400).json({ error: "Customer not found"});
+    }
+
+    request.customer = customer;
+    return next();
+
+}
+
 //Possivel criar conta e não é possivel cadastar um CPF já existente.
 app.post("/account", (request, response) => {
     const {cpf, name}  = request.body;
@@ -21,22 +35,16 @@ if (customerAlreadyExists){
         id: uuidv4(),
         statement:[]
     });
-    return response.status(201).send();
 
+    return response.status(201).send();
 });
 
+//app.use(verifyIfExistsAccountCPF);
+
 //possível buscar extrato bancário do cliente, não deve ser possível buscar extrato em uma conta não existente
-app.get("/statement/:cpf", (request, response) => {
-    const {cpf} = request.params;
-    const customer = customers.find(customer => customer.cpf === cpf);
-
-    if(!customer){
-        return response.status(400).json({ error: "Customer not found"});
-    }
-
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
     return response.json(customer.statement);
-})
-
-
+});
 
 app.listen(3333);
